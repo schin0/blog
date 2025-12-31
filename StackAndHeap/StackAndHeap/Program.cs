cd9f3c9932c96e164a05fa06bfdf300800fcb8df
Gabriel Schincariol <gschin11@gmail.com>
2025-12-30 22:17:47 -0300

    add stack and heap examples

--- StackAndHeap/StackAndHeap/Program.cs
+++ StackAndHeap/StackAndHeap/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-    
+﻿namespace StackAndHeap;
+
+internal class Program
+{
+    #region [Example Stack] Stack frames and function calls
+    int Multiply(int x, int y)
+    {
+        int result = x * y;
+        return result;
+    }
+    #endregion
+
+    #region [Example Stack] Recursion and stack overflow
+    int Factorial(int n)
+    {
+        if (n == 1)
+            return 1;
+
+        return n * Factorial(n - 1);
+    }
+    #endregion
+
+    #region [Example Heap] Objects living beyond a function
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+    Person CreatePerson()
+    {
+        return new Person { Name = "Maria" };
+    }
+    #endregion
+
+    #region [Example] Logical memory leaks
+    static List<Person> cache = new();
+
+    void AddPerson()
+    {
+        cache.Add(new Person());
+    }
+    #endregion
+}
---
+++ StackAndHeap/StackAndHeap/README.md
@@ -0,0 +1,2 @@
+﻿For more information on the differences between stack and heap memory allocation, you can refer to this article:
+https://medium.com/@gschin11/stack-vs-heap-51d997b08f4d
